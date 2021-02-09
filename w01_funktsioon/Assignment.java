3754bb9e299c9e5bc9b03dd0e7ef2c79a62b007c
Gaspar Luik <luik.gaspar@gmail.com>
2021-02-09 17:08:04 +0200

    Väike arendus
    Proovisin midagi aretada siin
    Note to self- Jürgensi git

--- w01_funktsioon/Assignment.java
+++ w01_funktsioon/Assignment.java
@@ -1,11 +1,26 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
 
 public class Assignment{
     public static void main(String[] args){
-        int weight, speed, equation;
+       int weight, speed, equation, i;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Speed: ");
+        String name = scanner.nextLine();
+        System.out.println("your weight: ");
 
         weight = 78;
-        speed = 40; // km/h
+        speed = 3; // km/h
         equation = weight * speed;
+        
+        while (i < speed){
+            System.out.println("ikka väiksem, kui 3");
+            ++i
+            if (i == 5){
+                System.out.println("I väärtus on nüüd 3!");
+            }
+        }
+
 
         if (equation > 3000){
             System.out.println(equation + "Njuutonit...."+"That is a lot of power!");
