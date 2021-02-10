f455dd2513c39681a14c561b81b7d397a52bbf60
Gaspar Luik <gasparluik@users.noreply.github.com>
2021-02-11 00:10:38 +0200

    Ülesanne valmis

--- w01_funktsioon/Assignment.java
+++ w01_funktsioon/Assignment.java
@@ -1,17 +1,34 @@
+import java.util.Scanner;
 
 public class Assignment{
     public static void main(String[] args){
-        int weight, speed, equation;
+        //Püstitan küsimuse 
+        String answer = "Ei";
+        System.out.println("Kas päevas on piisavalt tunde?");
 
-        weight = 78;
-        speed = 40; // km/h
-        equation = weight * speed;
+        //aktiveerin scanneri, mis scannib tähemärke??
+        Scanner scanner = new Scanner(System.in);
+        //nextLine() loeb järgnevat inputi(scanner)
+        String input = scanner.nextLine();
 
-        if (equation > 3000){
-            System.out.println(equation + "Njuutonit...."+"That is a lot of power!");
-        } else{
-            System.out.println("Sinu jõud kukkumisel on: " + equation + " Njuutonit");
+        //Klassikaline if else statement
+        if (answer.toLowerCase().equals(input.toLowerCase())){  // toLowerCase() annab kasutajale veidi eksimisruumi (Ei või ei, pole vahet)
+            System.out.println("Väga õige, nii palju on ju teha");
+        } else {
+            System.out.println("Ei nõustu, aga palun põhjenda enda otsust");
+            // Teen uue scanneri ja inputi
+            Scanner explanation = new Scanner(System.in);
+            String input2 = explanation.nextLine();
+            System.out.println("If you say so....");
+            
+            // Proovisin teha, et kasutaja ei saa vastuseks tühjust jätta aga ei oska :(
+            /*if (input2 == true){
+                System.out.println("If you say so....");
+            } else{
+                System.out.println("Sa ei põhjendanud end, kirjuta kasvõi midagigi");
+                Scanner explanation2 = new Scanner(System.in);
+                String input3 = explanation2.nextLine();
+            } */
         }
-
     }
 }
