8c87197f82c5b3db7c28619ee523864ff8e033a2
Gaspar Luik <70939487+gasparluik@users.noreply.github.com>
2021-05-14 12:07:59 +0300

    first

---
+++ w11_proovikontrolltoo/Joogidpudel.java
@@ -0,0 +1,11 @@
+public class Joogidpudel extends Jook {
+    public Joogidpudel(String nimetus, double omaLiiter, double erikaal) {
+        super(nimetus, omaLiiter, erikaal);
+    }
+    
+    double maht; //liitrites
+    String pudelityyp;
+    double mass;
+    double taaraMaks;
+
+}
---
+++ w11_proovikontrolltoo/Jook.java
@@ -0,0 +1,12 @@
+public abstract class Jook {
+    String nimetus;
+    double omaLiiter;
+    double erikaal;
+
+    public Jook (String nimetus, double omaLiiter, double erikaal){
+        this.nimetus = nimetus;
+        this.omaLiiter = omaLiiter;
+        this.erikaal = erikaal;
+    }
+    
+}
