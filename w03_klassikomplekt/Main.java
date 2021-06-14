4e6b3c40ccfa32ee1fa0182a8c5231145f94b054
Gaspar Luik <luik.gaspar@gmail.com>
2021-06-14 09:31:07 +0300

    Update Main.java

--- w03_klassikomplekt/Main.java
+++ w03_klassikomplekt/Main.java
@@ -6,8 +6,6 @@ public class Main {
 
     public static void main(String[] args){
 
-        String valid = "jah";
-
 
         ArrayList<Refrigerator> lemmikud = new ArrayList<>();
         ArrayList<Refrigerator> eu = new ArrayList<>();
@@ -26,22 +24,26 @@ public class Main {
         String warehouse = input.nextLine();
 
 
-        if (warehouse.equalsIgnoreCase("eu") | warehouse.equalsIgnoreCase("euroopa")){
-            eu.add(new Refrigerator(brand, model));
-        } if (brand.equalsIgnoreCase("Bosch")){
+        if (brand.equalsIgnoreCase("Bosch")){
             lemmikud.add(new Refrigerator(brand, model));
         } if(warehouse.equalsIgnoreCase("mujal")){
             nonEU.add(new Refrigerator(brand, model));
+        } else if(warehouse.equalsIgnoreCase("eu")){
+            eu.add(new Refrigerator(brand, model));
         } else{
             System.out.println("Sellist asukohta ei eksisteeri");
         }
 
 
         System.out.println("Kas soovid jätkata? [jah/ei]: ");
-        valid = input.nextLine();
+        String valid = input.nextLine();
 
-        System.out.println(eu);
-        System.out.println(lemmikud);
+        for(int i = 0; i < eu.size(); i++){
+            System.out.println(eu.get(i));
+        }
+
+        //System.out.println(eu.get(Refrigerator : x);
+        System.out.println(nonEU);
 
 
         input.close();
